0c772320f3f1baeeb31a3136f2404c5c10a2af90
Maffeys <matiusz98@gmail.com>
2019-12-09 16:50:53 -0500

    Network scanner stub, now more low-level programmed

--- network_scanner.py
+++ network_scanner.py
@@ -1,8 +1,15 @@
 #! /usr/bin/env python
 # network_scanner.py - A network scanner which uses Scapy to find out info about a network.
+# Python 2.7 compatible.
 
 import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast_frame = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_packet = broadcast_frame / arp_request
+    answered, unanswered = scapy.srp(arp_packet, timeout=1)
+
+
+scan("10.0.2.1/24")
