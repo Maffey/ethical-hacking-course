b92df721baa39aa5cc5a6019c4f98cf4d577aa46
Maffeys <matiusz98@gmail.com>
2019-12-09 18:32:40 -0500

    small tweaks

--- network_scanner.py
+++ network_scanner.py
@@ -9,7 +9,12 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast_frame = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_packet = broadcast_frame / arp_request
-    answered, unanswered = scapy.srp(arp_packet, timeout=1)
+    answered = scapy.srp(arp_packet, timeout=1, verbose=False)[0]
+
+    for answer in answered:
+        print(answer[1].psrc)
+        print(answer[1].hwsrc)
+        print("".center(32, "="))
 
 
 scan("10.0.2.1/24")
