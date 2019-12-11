15c30d4cc810ce9fe6f0673c6ae658481f16073e
Maffeys <matiusz98@gmail.com>
2019-12-11 17:30:03 -0500

    formatting tweaks

--- network_scanner.py
+++ network_scanner.py
@@ -10,11 +10,12 @@ def scan(ip):
     broadcast_frame = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_packet = broadcast_frame / arp_request
     answered = scapy.srp(arp_packet, timeout=1, verbose=False)[0]
-
+    
+    print("".center(33, "="))
+    print("IP\t\t\t\tMAC Address")
+    print("".center(33, "="))
     for answer in answered:
-        print(answer[1].psrc)
-        print(answer[1].hwsrc)
-        print("".center(32, "="))
+        print(answer[1].psrc + "\t\t" + answer[1].hwsrc)
 
 
 scan("10.0.2.1/24")
