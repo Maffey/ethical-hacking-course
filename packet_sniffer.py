c523f95fe10d48885aeb29c0caf5bb5d32ee9ed3
Maffeys <matiusz98@gmail.com>
2020-01-13 18:34:05 -0500

    packet sniffer stub

---
+++ packet_sniffer.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+import scapy.all as scapy
+from scapy.layers import http
+
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        print(packet)
+
+
+sniff("eth0")
