b4a12f588111335e6e00b75dd39923246a8be156
Maffey <matiusz98@gmail.com>
2020-03-05 17:02:45 -0500

    added file_interceptor.py file

---
+++ file_interceptor.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# WARNING: Not compatible with Python 3.x
+
+# TODO: add a simple comments paragraph with instructions about iptable and ip forwarding
+
+import netfilterqueue
+import scapy.all as scapy
+
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            print("HTTP Request")
+            print(scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport == 80:
+            print("HTTP Response")
+            print(scapy_packet.show())
+
+    packet.accept()
+
+
+print("Queue has been created.")
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
