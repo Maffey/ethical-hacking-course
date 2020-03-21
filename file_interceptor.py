2c42fcc4ec041f3d31d1d7a46689e5ff1adf5bee
Maffey <matiusz98@gmail.com>
2020-03-20 20:07:15 -0400

    further work on file_interceptor.py

--- file_interceptor.py
+++ file_interceptor.py
@@ -4,16 +4,22 @@
 import netfilterqueue
 import scapy.all as scapy
 
+ack_list = []
+
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
-            print("HTTP Request")
-            print(scapy_packet.show())
+            if ".exe" in scapy_packet[scapy.Raw].load:
+                print("[+] .EXE Request")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+                print(scapy_packet.show())
         elif scapy_packet[scapy.TCP].sport == 80:
-            print("HTTP Response")
-            print(scapy_packet.show())
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)  # Could be converted to walrus assignment in 3.8.
+                print("[+] Replacing file")
+                print(scapy_packet.show())
 
     packet.accept()
 
