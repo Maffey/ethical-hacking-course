835577475da8b4f168550bf14311dc542fbfde50
Maffey <matiusz98@gmail.com>
2020-04-17 21:34:45 +0200

    improvements on downloader.py

--- downloader.py
+++ downloader.py
@@ -5,7 +5,9 @@ import requests
 
 def download(url):
     get_response = requests.get(url)
-    print(get_response.content)
+    file_name = url.split("/")[-1]
+    with open("resources/" + file_name, "wb") as file:
+        file.write(get_response.content)
 
 
 download("https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Kotlin-logo.svg/512px-Kotlin-logo.svg.png")
